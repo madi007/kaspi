6be28ed1d02b3890674f518ce701d7c90b599f22
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-24 17:09:41 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,31 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
-      ]);
-    })
-  );
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/idcard.html',
+    'styles/style.css',
+    'styles/adcard.css', 
+    'scripts/script.js', 
+    'scripts/idcard.js', 
+    '/app.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
