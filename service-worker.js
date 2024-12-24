a1efeedbf409f4606f4da9ea6a86bdf08eac8def
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-24 18:19:51 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
     '/',
-    '/index.html',
-    '/idcard.html',
+    'index.html',
+    'idcard.html',
     'styles/style.css',
     'styles/adcard.css', 
     'scripts/script.js', 
     'scripts/idcard.js', 
-    '/app.js',
-    '/manifest.json'
+    'app.js',
+    'manifest.json'
 ];
 
 self.addEventListener('install', event => {
