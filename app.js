fbec9f4eb2b8db0898d0550f547d7225991542d5
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-25 05:13:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,33 @@ if ('serviceWorker' in navigator) {
         .then(() => console.log('Service Worker Registered'))
         .catch(err => console.error('Service Worker Error:', err));
 }
+
+// Обновить спискок документов
+function resetData() {
+    localStorage.clear();
+    alert("Все данные успешно сброшены!");
+}
+
+// Функция для проверки данных и сброса интерфейса
+function checkAndResetFields(fields, uploadedImageId, imageContainerId) {
+    const uploadedImage = document.getElementById(uploadedImageId);
+    const imageContainer = document.getElementById(imageContainerId);
+
+    // Проверяем, есть ли данные
+    if (!localStorage.getItem(fields[0])) {
+        // Сбрасываем поля
+        fields.forEach(field => {
+            const input = document.getElementById(field);
+            if (input) {
+                input.value = ''; // Очищаем значение
+                input.classList.remove('readonly'); // Убираем стили "только для чтения"
+            }
+        });
+
+        // Сбрасываем изображение
+        if (uploadedImage && imageContainer) {
+            uploadedImage.src = '';
+            imageContainer.style.display = 'none'; // Скрываем контейнер
+        }
+    }
+}
