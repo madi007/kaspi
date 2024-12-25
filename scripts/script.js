47216447727bd85a457d66f0dc03fc4b3e7dfefc
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-25 17:05:17 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
-document.getElementById("update-button").addEventListener("click", () => {
-    alert("Список документов обновлен!");
+document.addEventListener("DOMContentLoaded", () => {
+    const uploadButton = document.querySelector(".update-btn");
+
+    uploadButton.addEventListener("click", () => {
+        // Устанавливаем сигнал сброса в localStorage
+        localStorage.setItem("resetIdCard", "true");
+        alert("Данные для сброса отправлены. Перезагрузи страницу idcard.html.");
+    });
 });
