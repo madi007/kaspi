57e6f7108617b94a9e9f2e5b7a91fa4d1988f781
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-28 03:10:28 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -46,7 +46,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => caches.match('kaspi/offline.html'));
+            return response || fetch(event.request).catch(() => caches.match('kaspi/idcard.html'));
         })
     );
 });
