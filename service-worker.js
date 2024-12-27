db9144afc501269b38a407e15610e2ac7c733e45
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2024-12-28 03:09:04 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,30 +8,45 @@ const urlsToCache = [
     'kaspi/styles/style.css',
     'kaspi/styles/idcard.css',
     'kaspi/styles/stucard.css',
-    'kaspi/styles/medcard.css', 
+    'kaspi/styles/medcard.css',
     'kaspi/scripts/script.js',
     'kaspi/scripts/stucard.js',
     'kaspi/scripts/medcard.js',
-    'kaspi/scripts/idcard.js', 
+    'kaspi/scripts/idcard.js',
     'kaspi/app.js',
-    'kaspi/manifest.json'
+    'kaspi/manifest.json',
+    'kaspi/offline.html' // Оффлайн-страница
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => caches.match('kaspi/offline.html'));
+        })
     );
 });
